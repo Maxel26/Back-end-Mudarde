19debe59652673219c57e369d743f78d15d319dd
unknown <Maxel26>
2023-07-24 20:17:39 -0500

    [+] categories.routes : Define rutas para / api/categories

---
+++ src/routes/categories.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require( 'express' );
+
+const  router = Router();
+
+//  http://localhost:3000/api/categories
+router.get('/',);
+router.post('/',);
+router.patch('/:id',);
+router.delete('/:id',);
+
+
+module.exports = router;
